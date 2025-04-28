9cdf4990db8c53a1b32ad6c4b595795102ea87fd
ShadowBih <putraalbi04@gmail.com>
2025-04-28 23:11:38 +0700

    Add aluminium-sales-website/config.php 1745856698439

---
+++ aluminium-sales-website/config.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection configuration
+$host = 'localhost';
+$dbname = 'aluminium_sales';
+$username = 'root';
+$password = '';
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
